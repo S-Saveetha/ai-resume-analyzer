c6b084790aca2398cc8f565a1688972fe923dea2
S-Saveetha <ssaveetha47@gmail.com>
2026-03-21 21:33:28 +0530

    Added PDF text extraction

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from parser.resume_parser import extract_text_from_pdf
 
 st.title("AI Resume Analyzer")
 
@@ -6,3 +7,8 @@ uploaded_file = st.file_uploader("Upload your resume", type=["pdf"])
 
 if uploaded_file is not None:
     st.write("File uploaded successfully!")
+
+    resume_text = extract_text_from_pdf(uploaded_file)
+
+    st.subheader("Extracted Resume Text")
+    st.text_area("Resume Content", resume_text, height=300)
